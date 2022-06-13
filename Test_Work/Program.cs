10befe83b2129fcc90d4b0c044a6d47766bd7e03
Dimoklim <dimoklim@ya.ru>
2022-06-14 00:45:42 +0300

    Add Test Work

---
+++ Test_Work/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше либо равна 3 символа.
+
+string[] array = { "123", "23", "hello", "world", "res" };
+
+void threeСharArray(string[] array)
+{
+    string[] newarr = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            newarr[i] = array[i]; ;
+            Console.Write($"{newarr[i]} ");
+        }
+    }
+}
+
+threeСharArray(array);
